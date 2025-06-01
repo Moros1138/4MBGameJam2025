ff065defc2e9e63c92a1bf35c819192a93a83879
Moros Smith <moros1138@gmail.com>
2025-05-31 21:49:15 -0400

    remove build artifacts from repo, dumbass

--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jon/Projects/Moros1138/4MB-Game-Jam-2025/raylib/src
-# Build directory: C:/Users/Jon/Projects/Moros1138/4MB-Game-Jam-2025/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
